7f0b2041265ab7e45b7ea89aa05b971110fa5fad
dantelentsoe <dllentsoe@gmail.com>
2022-06-27 19:32:17 +0200

    feat: add ts to the server

--- server/index.ts
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-require("dotenv").config();
-
-// import { express } from "express";
-const port = process.env.PORT || 5000;
-const app = express();
---
+++ server/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("GraphQL Server");
+});
+
+app.listen(port, () => console.log(`Server running on ... ${port}`));
