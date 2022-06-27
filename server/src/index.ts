c8c92f4b3921082cb67b9c02200aa6355ed0159a
dantelentsoe <dllentsoe@gmail.com>
2022-06-27 20:52:08 +0200

    feat: setup graphql playground

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express, { Express, Request, Response } from "express";
+import { graphqlHTTP } from "express-graphql";
+const schema = require("./schema");
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -10,5 +12,13 @@ const port = process.env.PORT || 8000;
 app.get("/", (req: Request, res: Response) => {
   res.send("GraphQL Server");
 });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
 
 app.listen(port, () => console.log(`Server running on ... ${port}`));
