12e72e6a97331ee6f008539ce34c566643f16ffa
dantelentsoe <dllentsoe@gmail.com>
2022-06-27 20:51:45 +0200

    feat: query types

---
+++ server/src/contants/types.ts
@@ -0,0 +1,14 @@
+export interface Projects {
+  id: string;
+  clientId: string;
+  name: string;
+  description: string;
+  status: string;
+}
+
+export interface Client {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+}
