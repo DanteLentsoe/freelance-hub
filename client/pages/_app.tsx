38e73f559b0f68343b44fff26bd84d7a48eeb5d6
dantelentsoe <dllentsoe@gmail.com>
2022-07-06 20:09:21 +0200

    feat: add graphql

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
