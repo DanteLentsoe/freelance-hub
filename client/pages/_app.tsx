a66d0099f8b85be50545fbc1a0225bdcfa64276b
dantelentsoe <dllentsoe@gmail.com>
2022-07-11 17:45:08 +0200

    refactor: update cache polices

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,19 +2,40 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { RecoilRoot } from "recoil";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
-  cache: new InMemoryCache(),
+  cache,
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <ChakraProvider>
-        <Component {...pageProps} />
-      </ChakraProvider>
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloProvider client={client}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </ApolloProvider>
+    </RecoilRoot>
   );
 }
 
