fcf2511c492d72b7592423a3b5f399b86956d246
dantelentsoe <dllentsoe@gmail.com>
2022-07-15 19:05:25 +0200

    feat: add search filter for projects component

--- client/components/ProjectContainer/index.tsx
+++ client/components/ProjectContainer/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, SetStateAction, useState } from "react";
 import {
   Box,
   Stack,
@@ -11,17 +11,25 @@ import {
   ListIcon,
   Button,
   useToast,
+  Container,
+  SimpleGrid,
   Center,
+  InputGroup,
+  InputRightElement,
+  Input,
+  InputLeftElement,
 } from "@chakra-ui/react";
 import { FaCheckCircle } from "react-icons/fa";
-import { GET_PROJECTS } from "../../graphql/queries/project";
+import Fuse from "fuse.js";
+import { GET_PROJECT, GET_PROJECTS } from "../../graphql/queries/project";
 import { useQuery } from "@apollo/client";
 import { IProject } from "../../contants/types";
 import Loader from "../UI/Loader";
 import { DataFectingErrorSVG } from "../../assets/SVG";
+import { CloseIcon, SearchIcon } from "@chakra-ui/icons";
 
 interface ProjectData {
-  clients: IProject[];
+  projects: IProject[];
 }
 
 const PriceWrapper = ({ children }: { children: ReactNode }) => {
@@ -40,6 +48,8 @@ const PriceWrapper = ({ children }: { children: ReactNode }) => {
 
 const ProjectContainer = () => {
   const { data, loading, error } = useQuery<ProjectData>(GET_PROJECTS);
+
+  // GET_PROJECT
 
   const toast = useToast();
   const [query, setQuery] = useState<string>("");
@@ -71,60 +81,121 @@ const ProjectContainer = () => {
     );
   }
 
-  console.log("DAta ", data);
+  console.log("DAta ", data?.projects);
+  const handleChange = (event: { target: { value: SetStateAction<string> } }) =>
+    setQuery(event.target.value);
+
+  const options = {
+    includeScore: true,
+    keys: ["name", "email"],
+  };
+
+  const projectsData = data && data?.projects;
+
+  const fuse = new Fuse(projectsData as readonly IProject[], options);
+
+  //query results
+  const results = fuse.search(query);
+
+  const projectResults = query
+    ? results.map((projects) => projects.item)
+    : projectsData;
 
   return (
-    <Box py={12}>
-      <Stack
-        direction={{ base: "column", md: "row" }}
-        textAlign="center"
-        justify="center"
-        spacing={{ base: 4, lg: 10 }}
-        py={10}>
-        <PriceWrapper>
-          <Box py={4} px={12}>
-            <Text fontWeight="500" fontSize="2xl">
-              Scale
-            </Text>
-            <HStack justifyContent="center">
-              <Text fontSize="3xl" fontWeight="600">
-                $
-              </Text>
-              <Text fontSize="5xl" fontWeight="900">
-                349
-              </Text>
-              <Text fontSize="3xl" color="gray.500">
-                /month
-              </Text>
-            </HStack>
-          </Box>
-          <VStack
-            bg={useColorModeValue("gray.50", "gray.700")}
-            py={4}
-            borderBottomRadius={"xl"}>
-            <List spacing={3} textAlign="start" px={12}>
-              <ListItem>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                unlimited build minutes
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                Lorem, ipsum dolor.
-              </ListItem>
-              <ListItem>
-                <ListIcon as={FaCheckCircle} color="green.500" />
-                5TB Lorem, ipsum dolor.
-              </ListItem>
-            </List>
-            <Box w="80%" pt={7}>
-              <Button w="full" colorScheme="red" variant="outline">
-                Start trial
-              </Button>
-            </Box>
-          </VStack>
-        </PriceWrapper>
+    <>
+      <Stack spacing={4}>
+        <Center>
+          <InputGroup w={300} marginBottom={10} top={5}>
+            <InputLeftElement
+              pointerEvents="none"
+              color="gray.300"
+              fontSize="1.2em"
+              // eslint-disable-next-line react/no-children-prop
+              children={<SearchIcon fontSize={20} />}
+            />
+            <Input
+              value={query}
+              onChange={handleChange}
+              placeholder="Search Clients"
+              size="lg"
+              w={300}
+              boxShadow={"md"}
+            />
+            {query.length > 0 && (
+              <InputRightElement
+                // eslint-disable-next-line react/no-children-prop
+                children={
+                  <CloseIcon
+                    fontSize={15}
+                    color={"gray"}
+                    onClick={() => {
+                      setQuery("");
+                    }}
+                    _hover={{
+                      cursor: "pointer",
+                    }}
+                  />
+                }
+              />
+            )}
+          </InputGroup>
+        </Center>
       </Stack>
-    </Box>
+      <Container maxW={"6xl"} mt={10}>
+        <SimpleGrid columns={{ base: 1, md: 2, lg: 4 }} spacing={10}>
+          {projectResults &&
+            projectResults.map((project: IProject) => {
+              return (
+                <Box py={12} key={project.id}>
+                  <Stack
+                    direction={{ base: "column", md: "row" }}
+                    textAlign="center"
+                    justify="center"
+                    spacing={{ base: 4, lg: 10 }}
+                    py={10}>
+                    <PriceWrapper>
+                      <Box py={4} px={12}>
+                        <Text fontWeight="500" fontSize="2xl">
+                          {project.name}
+                        </Text>
+                        <HStack justifyContent="center">
+                          <Text fontSize="3xl" fontWeight="600">
+                            $
+                          </Text>
+                          <Text fontSize="5xl" fontWeight="900">
+                            {project.amount}
+                          </Text>
+                          <Text fontSize="3xl" color="gray.500">
+                            {project.client?.email}
+                          </Text>
+                        </HStack>
+                      </Box>
+                      <VStack py={4} borderBottomRadius={"xl"}>
+                        <List spacing={3} textAlign="start" px={12}>
+                          <ListItem>
+                            <ListIcon as={FaCheckCircle} color="green.500" />
+                            {project.description}
+                          </ListItem>
+
+                          <ListItem>
+                            <ListIcon as={FaCheckCircle} color="green.500" />
+                            {project.status}
+                          </ListItem>
+                        </List>
+                        <Box w="80%" pt={7}>
+                          <Button w="full" colorScheme="red" variant="outline">
+                            {project.completed}
+                          </Button>
+                        </Box>
+                      </VStack>
+                    </PriceWrapper>
+                  </Stack>
+                </Box>
+              );
+            })}
+        </SimpleGrid>
+      </Container>
+    </>
   );
 };
 
---
+++ client/pages/freelanceprojects.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import ClientsContainer from "../components/clientcontainer";
+import AddProjectModal from "../components/modals/AddProjectModal";
+import NavigationBar from "../components/NavigationBar";
+import ProjectContainer from "../components/ProjectContainer";
+
+const FreeLanceProjectPage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>FreeLance Hub | ProjectPage</title>
+        <meta
+          name="description"
+          content="FreeLance Hub is platform designed for frelances to track and document their projects"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <NavigationBar />
+      <ProjectContainer />
+      <AddProjectModal />
+    </>
+  );
+};
+
+export default FreeLanceProjectPage;
