e0d83196fc6ac95b4f2f3b233323b3070b3cdacf
dantelentsoe <dllentsoe@gmail.com>
2022-07-21 21:48:56 +0200

    refactor: update dynamic sizing of project details

--- client/pages/projects/[id].tsx
+++ client/pages/projects/[id].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import type { NextPage } from "next";
 import Head from "next/head";
 import NavigationBar from "../../components/NavigationBar";
@@ -13,11 +14,11 @@ import {
   Heading,
   Text,
   Stack,
-  Avatar,
   useToast,
   useColorModeValue,
-  Image,
+  Button,
 } from "@chakra-ui/react";
+import ClientProjectCard from "../../components/clientProjectCard";
 
 interface ISinglProject {
   project: IProject;
@@ -74,49 +75,55 @@ const SingleProject: NextPage = () => {
       <NavigationBar />
       <Center py={6}>
         <Box
-          maxW={"445px"}
-          w={"full"}
-          // bg={useColorModeValue("white", "gray.900")}
+          maxW={"80%"}
+          bg={useColorModeValue("white", "gray.900")}
           boxShadow={"2xl"}
           rounded={"md"}
           p={6}
           overflow={"hidden"}>
           <Box
-            h={"210px"}
-            bg={"gray.100"}
+            h={"60px"}
+            bg={useColorModeValue("gray.100", "gray.400")}
             mt={-6}
             mx={-6}
             mb={6}
-            pos={"relative"}>
-            {/* <Image
-              src={
-                "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
-              }
-              layout={"fill"}
-            /> */}
-          </Box>
-          <Stack>
-            <Text
-              color={"green.500"}
-              textTransform={"uppercase"}
-              fontWeight={800}
-              fontSize={"sm"}
-              letterSpacing={1.1}>
+            pos={"relative"}></Box>
+
+          <Stack
+            p={3}
+            py={3}
+            justifyContent={{
+              base: "flex-start",
+              md: "space-around",
+            }}
+            direction={{
+              base: "column",
+              md: "row",
+            }}
+            alignItems={{ md: "center" }}>
+            <Heading size={"lg"} color={"green.500"} letterSpacing={1.1}>
               {data?.project.name as string}
-            </Text>
+            </Heading>
+
             <Heading
               // color={useColorModeValue("gray.700", "white")}
-              fontSize={"2xl"}
+              fontSize={"md"}
               fontFamily={"body"}>
-              Completed: {data?.project.completed}
+              Completed:
             </Heading>
-            <Text color={"gray.500"}>{data?.project.description}</Text>
+            {data?.project.completed ? (
+              <Text>Done</Text>
+            ) : (
+              <Text>Not Done</Text>
+            )}
+            <Heading size={"md"}>{"typePlan"}</Heading>
+            <Stack>
+              <Button size="md">Edit Details</Button>
+            </Stack>
           </Stack>
+
+          <Text color={"gray.500"}>{data?.project.description}</Text>
           <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
-            <Avatar
-              src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
-              alt={"freelance hub project image"}
-            />
             <Stack direction={"column"} spacing={0} fontSize={"sm"}>
               <Text fontWeight={600}>
                 Client : {data?.project.client?.name}
@@ -124,6 +131,7 @@ const SingleProject: NextPage = () => {
               <Text color={"gray.500"}>Feb 08, 2021 · 6min read</Text>
             </Stack>
           </Stack>
+          <ClientProjectCard />
         </Box>
       </Center>
     </>
