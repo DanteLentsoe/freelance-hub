c2baf85ffac52a83ee381f91a1874ddde92e0a85
dantelentsoe <dllentsoe@gmail.com>
2022-07-24 19:11:38 +0200

    refactor: update client details info display

--- client/components/clientProjectCard/index.tsx
+++ client/components/clientProjectCard/index.tsx
@@ -49,75 +49,77 @@ const ClientProjectCard = ({
 }: IClientProjectCard) => {
   return (
     <Box py={6} px={5} minW={"100vh"}>
-      {clientData === null ? (
-        <Heading size={"md"} textAlign={"center"}>
-          <Text>No Client Assigned To Project</Text>
-        </Heading>
-      ) : (
-        <Stack spacing={4} width={"100%"} direction={"column"}>
+      <Stack spacing={4} width={"100%"} direction={"column"}>
+        <Stack
+          p={5}
+          alignItems={"center"}
+          justifyContent={{
+            base: "flex-start",
+            md: "space-around",
+          }}
+          direction={{
+            base: "column",
+            md: "row",
+          }}>
           <Stack
-            p={5}
-            alignItems={"center"}
-            justifyContent={{
-              base: "flex-start",
-              md: "space-around",
+            width={{
+              base: "100%",
+              md: "40%",
             }}
-            direction={{
-              base: "column",
-              md: "row",
-            }}>
-            <Stack
-              width={{
-                base: "100%",
-                md: "40%",
-              }}
-              textAlign={"center"}>
-              <Heading size={"md"}>
-                <Text color="purple.400">Notes :</Text>
-              </Heading>
-            </Stack>
-            <Stack
-              width={{
-                base: "100%",
-                md: "60%",
-              }}>
-              <Text textAlign={"center"}>{projectNotes}</Text>
-            </Stack>
+            textAlign={"center"}>
+            <Heading size={"md"}>
+              <Text color="purple.400">Notes :</Text>
+            </Heading>
           </Stack>
-          <Divider />
-          <PackageTier
-            title={"Project Status"}
-            value={projectStatus as string}
-          />
-          <Divider />
-          <PackageTier
-            title={"Amount"}
-            checked={true}
-            value={amount as string}
-          />
-          <Divider />
-          <PackageTier
-            title={"Client Name"}
-            value={clientData?.name as string}
-          />
-          <Divider />
-          <PackageTier
-            title={"Client Email"}
-            value={clientData?.email as string}
-          />
-          <Divider />
-
-          <PackageTier
-            title={"Client Phone"}
-            value={clientData?.phone as string}
-          />
-          <Divider />
-          <PackageTier
-            title={"Client Notes"}
-            value={clientData?.clientNote as string}
-          />
+          <Stack
+            width={{
+              base: "100%",
+              md: "60%",
+            }}>
+            <Text textAlign={"center"}>{projectNotes}</Text>
+          </Stack>
         </Stack>
-      )}
+        <Divider />
+        {clientData === null ? (
+          <Heading size={"md"} textAlign={"center"}>
+            <Text>No Client Assigned To Project</Text>
+          </Heading>
+        ) : (
+          <>
+            <PackageTier
+              title={"Project Status"}
+              value={projectStatus as string}
+            />
+            <Divider />
+            <PackageTier
+              title={"Amount"}
+              checked={true}
+              value={amount as string}
+            />
+            <Divider />
+            <PackageTier
+              title={"Client Name"}
+              value={clientData?.name as string}
+            />
+            <Divider />
+            <PackageTier
+              title={"Client Email"}
+              value={clientData?.email as string}
+            />
+            <Divider />
+
+            <PackageTier
+              title={"Client Phone"}
+              value={clientData?.phone as string}
+            />
+            <Divider />
+            <PackageTier
+              title={"Client Notes"}
+              value={clientData?.clientNote as string}
+            />
+          </>
+        )}
+      </Stack>
     </Box>
   );
 };
